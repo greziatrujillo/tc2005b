a4f4a4e0ad177df8177e533648b73fc87f025ca9
Grezia Trujillo <a01713876@tec.mx>
2026-03-30 21:17:03 -0600

    Created function for the prompt in javascript. Generated 2 random numbers from 0 to 100 and start taking time.

--- main_ex2.js
+++ main_ex2.js
@@ -3,9 +3,16 @@
 //1. creating a table
 
 function generateSum() {
-    //Prompt user for answer
+    //Generate 2 random nums for sum
+    const num1 = Math.floor(Math.random() * 101);
+    const num2 = Math.floor(Math.random() * 101);
 
-    //Generate 2 random nums
+    const correct = num1 + num2;
+
+    //time taken to answer
+    const time = Date.now();
+
+    //Prompt user for answer
 
     //answer is correct or incorrect with time taken to answer
 
