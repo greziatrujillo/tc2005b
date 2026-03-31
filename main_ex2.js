f429b9c9d783e407068819815b09fff8df83ebd6
Grezia Trujillo <a01713876@tec.mx>
2026-03-30 21:08:47 -0600

    Created HTML and JS files for exercise 2. HTML file has corresponding information. JS setup to modify to fir exercise parameters.

---
+++ main_ex2.js
@@ -0,0 +1,13 @@
+//JS lab file exercise 2
+
+//1. creating a table
+
+function generateSum() {
+    //Prompt user for answer
+
+    //Generate 2 random nums
+
+    //answer is correct or incorrect with time taken to answer
+
+
+}
