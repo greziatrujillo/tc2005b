46f2861cea786e4b22f3e44c623a2793e75678f5
Grezia Trujillo <a01713876@tec.mx>
2026-03-30 19:05:53 -0600

    Created the space for the first exercise. Outlined the steps for the first exercise in the JS file.

---
+++ main.js
@@ -0,0 +1,9 @@
+//JS lab file 
+
+//Prompt user for number
+
+//validate the character entered is a number
+
+//start creating table using document.write
+
+//loop will create rows and columns with repective information
