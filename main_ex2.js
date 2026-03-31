24aa3145307de3530e2be5f6574830e08de80eca
Grezia Trujillo <a01713876@tec.mx>
2026-03-30 21:28:56 -0600

    Edited some variables names and comments. Polished the code.

--- main_ex2.js
+++ main_ex2.js
@@ -1,26 +1,27 @@
 //JS lab file exercise 2
 
-//1. creating a table
+//2. Generate random sum
 
 function generateSum() {
     //Generate 2 random nums for sum
+    //numbers will be whole numbers between 0 and 100
     const num1 = Math.floor(Math.random() * 101);
     const num2 = Math.floor(Math.random() * 101);
 
     const correct = num1 + num2;
 
-    //time taken to answer
-    const time = Date.now();
+    //start time taken to answer
+    const timeInit = Date.now();
 
     //Prompt user for answer
     const answer = prompt ('Responde la suma: ' + num1 + ' + ' + num2);
 
-    //Final time taken
-    const finalTime = Date.now() - time;
+    //Final time taken to answer
+    const finalTime = Date.now() - timeInit;
 
-    //answer is correct or incorrect with time taken to answer
+    //answer is correct with time taken to answer or incorrect 
     if (answer == correct) {
-        alert('Correcto! Tiempo tomado en responder: ' + finalTime / 1000 + ' segundos');
+        alert('Correcto! Tiempo tomado en responder: ' + finalTime + ' segundos');
     } else {
         alert('Incorrecto! La respuesta correcta es: ' + correct);
     }
