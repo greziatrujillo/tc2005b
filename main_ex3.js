f0b5d7c9005fce17c54d07fb6fd0e65071902caa
Grezia Trujillo <a01713876@tec.mx>
2026-04-01 22:40:33 -0600

    Created counter to take in negatives, positives and zeros.

--- main_ex3.js
+++ main_ex3.js
@@ -2,11 +2,12 @@
 
 //3. Generate counter to identify properties of numbers 
 
-function counter() {
+function counter(array) {
+    let counter = {
+        neg: 0,
+        zero: 0,
+        pos: 0
+    };
 
 
-
-
-
-    
 }
