d989da2835bf06d4ecec196da362a96705ea1e09
Grezia Trujillo <a01713876@tec.mx>
2026-04-07 11:16:19 -0600

    First version of javascript file. Includes a function that prompts the user to input digits and the function will run through the digits. Unable to display yet.

--- main_ex5.js
+++ main_ex5.js
@@ -3,6 +3,12 @@
 //5. Receive a string of digits and return the inverse
 
 function inverseDigits(n) {
-    
+    var num = prompt ("Enter a number: ");
+    var result = "";
+    for (var i = num.length - 1; i >= 0; i--) {
+        result += num[i];
+    }
+
+    return result;
 }
 
