0ea9c812da35ef4545ca00424364f2a186fc38a9
Grezia Trujillo <a01713876@tec.mx>
2026-04-08 12:42:05 -0600

    Added HTML to server that has been created.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 let log = console.log;
 
-//creando un servidor
-const http = require("http");
-const server = http.createServer((request, response) =>{ 
-    log("request received");
+const http = require('http');
+const server=http.createServer((req, res) => {
+    log(req.url);
+    res.setHeader("content-type", "text/html");
+    res.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>HTML</title>
+        </head>
+        <body>
+        <h1>:3</h1>
+        </body>
+        </html>
+    `);
+    res.end();
 });
 
-server.listen(3000);
+server.listen(4141, () => {
+    log("Mi servidor está vivo corriendo en el puerto 4141");
+});
