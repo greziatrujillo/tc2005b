07a490fab0897b3862c863dbda2a64b96a463bec
Grezia Trujillo <a01713876@tec.mx>
2026-04-07 21:22:19 -0600

    Connected function to button on HTML and created an instance of the class.

--- main_ex6.js
+++ main_ex6.js
@@ -17,6 +17,23 @@ class tipCalculator {
     }
 }
 
+//connect the function to the button in the HTML
+function calculateTip() {
+    const totalCost = document.getElementById("totalCuenta").value;
+    const perc = document.getElementById("propina").value;
 
+    //verify there is an input for the cost
+    if (totalCost === "") {
+        alert("Por favor, ingresa el total de la cuenta.");
+        return;
+    }
+
+    //create an instance of the tipCalculator class
+    const calculator = new tipCalculator(parseFloat(totalCost), parseFloat(perc));
+    const tip = calculator.calculateTip();
+
+    //display the result
+    alert(`La propina es de ${tip.toFixed(2)} MXN.`);
+}
 
 
