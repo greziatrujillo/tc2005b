e9645cb895c5bdf22bd2aaaf3eb2e69274bc0fa5
Grezia Trujillo <a01713876@tec.mx>
2026-04-08 11:57:41 -0600

    Added file system library to be able to manage files or create them. As an example, a txt file was created with the message hello world.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("Hello, world!");
+let log = console.log;
 
+log("Hello, world!");
+
+
+//fs es el modulo que va contener las funciones para 
+//manipular el sistema de archivos
+
+let fs = require("fs");
+
+//crear un archivo con la clase writeFileSync
+fs.writeFileSync("archivo.txt", "Hola mundo");
