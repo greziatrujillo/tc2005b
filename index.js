a21a57907f0b8ef7caa8143a2fec4358bcdccf7e
Grezia Trujillo <a01713876@tec.mx>
2026-04-13 13:14:41 -0600

    worked on connecting form and adding other routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path    = require('path');
 const fs      = require('fs');
 const app     = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
@@ -45,38 +48,8 @@ app.get('/test_html', (request, response, next) => {
     response.end(); 
 });
 
-app.get('/form_method', (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');
-    const html = fs.readFileSync(path.resolve(__dirname, './form.html'), 'utf8')
-    response.write(html);
-    response.end();  
-});
-
-app.post('/form_method', (request, response, next) => {
-    let body = [];
-    request
-    .on('data', chunk => {
-        body.push(chunk);
-    })
-    .on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body)
-
-        const indice = Number(body.split('&')[0].split('=')[1]);
-        console.log(indice);
-        const imprimir = body.split('&')[1].split('=')[1];
-        console.log(imprimir);
-
-        for(var i = 1; i <= indice; i++){
-            console.log(imprimir)
-        }
-
-        response.setHeader('Content-Type', 'application/json');
-        response.statusCode = 200;
-        response.write('{code:200, msg:"Ok POST"}');
-        response.end();
-    }); 
-});
+const rutasFormulario = require("./formulario.routes");
+app.use('/formulario', rutasFormulario);
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
